065c5746ebbf7ae0a7025dd3446d56b40692623f
ycluo <imqdcn@gmail.com>
2026-04-08 15:52:04 +0800

    [feat] 使用@strapi/provider-upload-aws-s3上传插件

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -11,6 +11,37 @@ const config: Core.Config.Middlewares = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  // 自定义安全中间件配置，添加 R2 域名到 CSP
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_CUSTOM_DOMAIN'), // 替换为你的 R2 公开域名
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            env('R2_CUSTOM_DOMAIN'), // 替换为你的 R2 公开域名
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
 
 export default config;
+function env(key: string): string {
+  return process.env[key] || '';
+}
+
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,30 @@
 import type { Core } from '@strapi/strapi';
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      rootPath: env('CDN_ROOT_PATH'),
+      providerOptions: {
+        baseUrl: env('R2_CUSTOM_DOMAIN'),
+        s3Options: {
+          credentials: {
+            accessKeyId: env('R2_ACCESS_KEY_ID'),
+            secretAccessKey: env('R2_ACCESS_SECRET'),
+          },
+          region: 'auto',
+          endpoint: env('R2_ENDPOINT'), // 例如 "https://<account-id>.r2.cloudflarestorage.com"
+          params: {
+            Bucket: env('R2_BUCKET'),
+            // 不要设置 ACL - R2 不支持 ACL
+          },
+        },
+      },
+      security: {
+        allowedTypes: ['image/*', 'video/*', 'application/*'],
+      },
+    },
+  },
+});
 
 export default config;
